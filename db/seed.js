3a414ec2145734b6fbe7b36d6ef0fd89b5e91eb8
DippinDot <jpmarino33@gmail.com>
2022-07-13 15:26:43 -0600

    finished updating our users table

--- db/seed.js
+++ db/seed.js
@@ -14,7 +14,7 @@
 //   }
 // }
 
-const { client, getAllUsers, createUser } = require("./index");
+const { client, getAllUsers, createUser, updateUser } = require("./index");
 
 // new function, should attempt to create a few users
 async function createInitialUsers() {
@@ -103,8 +103,16 @@ async function testDB() {
   try {
     console.log("Starting to test database...");
 
+    console.log("Calling getAllUsers");
     const users = await getAllUsers();
-    console.log("getAllUsers:", users);
+    console.log("Result:", users);
+
+    console.log("Calling updateUser on users[0]");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY",
+    });
+    console.log("Result:", updateUserResult);
 
     console.log("Finished database tests!");
   } catch (error) {
