4bb7fe7dd291a615a44f8f1f7c7fc6d025b0595d
bigzepfan <dlcandassociates@gmail.com>
2022-07-19 14:31:51 -0600

    Finished Part II

--- db/index.js
+++ db/index.js
@@ -316,6 +316,25 @@ async function getPostsByTagName(tagName) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 // and export them
 module.exports = {
   client,
@@ -333,4 +352,5 @@ module.exports = {
   getPostsByUser,
   getPostsByTagName,
   getAllTags,
+  getUserByUsername,
 };
